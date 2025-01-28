eb4374ce1e09d07b6415a6796f31c99ef472a354
Ayham Boucher <ayhamboucher@gmail.com>
2025-01-28 12:37:32 -0500

    switch to LLM Gateway

--- Chatbot.py
+++ Chatbot.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from openai import OpenAI
-from os import environ
 
 st.title("Chatbot")
 st.caption("Powered by INFO-5940")
@@ -9,7 +8,8 @@ if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Hello! How can I help you today?"}]
 
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+    st.write(f"{msg['role']}: {msg['content']}")
+
 
 if prompt := st.chat_input():
 
@@ -18,7 +18,7 @@ if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
-    response = client.chat.completions.create(model="gpt4o", messages=st.session_state.messages)
+    response = client.chat.completions.create(model="openai.gpt-4o", messages=st.session_state.messages)
 
     msg = response.choices[0].message.content
 
